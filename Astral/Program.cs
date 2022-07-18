f73f127e826cdb008a5764541bb32bae0f569697
AlizerLaptop <floydguevarra58@gmail.com>
2022-07-18 15:37:35 +0800

    Add project files.

---
+++ Astral/Program.cs
@@ -0,0 +1,45 @@
+﻿using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using Tensorflow.NumPy;
+
+using static SharpCV.Binding;
+
+namespace Astral
+{
+    public class Program
+    {
+        private ScreenGrab screenGrab = new ScreenGrab(2, 15, Screen.PrimaryScreen);
+
+        public async Task StartAsync()
+        {
+            Console.WriteLine($"Program started {DateTime.Now}");
+
+            screenGrab.Screenshot += OnScreenshot;
+
+            Console.WriteLine($"Vision started {DateTime.Now}");
+
+            var oneSecondTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+            _ = Task.Run(async () =>
+            {
+                while (await oneSecondTimer.WaitForNextTickAsync())
+                {
+                    Console.WriteLine($"{DateTime.Now} - {screenshots} fps");
+                    screenshots = 0;
+                }
+            });
+
+            await screenGrab.StartPeriodicScreenshotAsync(); // Run async.
+        }
+
+        private int screenshots = 0;
+
+        private void OnScreenshot(object? sender, SharpCV.Mat e)
+        {
+            screenshots++;
+        }
+
+        static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+    }
+}
---
+++ Astral/ScreenGrab.cs
@@ -0,0 +1,71 @@
+﻿using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Astral.ScreenInfo;
+using static SharpCV.Binding;
+
+namespace Astral
+{
+    public class ScreenGrab
+    {
+        private readonly int scale;
+        private readonly int delay;
+        private bool doScreenshot = true;
+        private PeriodicTimer timer;
+
+        /// <summary>
+        /// Screenshot utility.
+        /// </summary>
+        /// <param name="downScale">Multiple size of the screenshot image to scale on, 2 will scale the image down to 50%.</param>
+        /// <param name="fps">Screenshot fps, setting this to 20 will make the program capture 20 screenshots per second, still depends on hardware speed.</param>
+        public ScreenGrab(int downScale, int fps, Screen screen)
+        {
+            this.scale = downScale;
+            this.delay = 1000 / fps;
+            this.timer = new PeriodicTimer(TimeSpan.FromMilliseconds(delay));
+
+            var scInfo = ScreenInfo.From(screen);
+
+            bm = new Bitmap(scInfo.PhysicalBounds.Width,
+                scInfo.PhysicalBounds.Height);
+        }
+
+        public event EventHandler<SharpCV.Mat>? Screenshot;
+
+        public async Task StartPeriodicScreenshotAsync()
+        {
+            while (doScreenshot && await timer.WaitForNextTickAsync())
+            {
+                var screenshot = GetSreenshot();
+
+                using (var cv2Screenshot = new SharpCV.Mat(screenshot.ToMat().CvPtr)) // Convert to SharpCV Mat object.
+                {
+                    using (var resized = cv2.resize(cv2Screenshot,
+                            ((int)cv2Screenshot.shape[1] / scale, (int)cv2Screenshot.shape[0] / scale)))
+                    {
+                        Screenshot?.Invoke(this, resized);
+                    }
+                }
+            }
+        }
+
+        Bitmap bm;
+
+        /// <summary>
+        /// Screenshots the screen.
+        /// </summary>
+        /// <returns>Bitmap of the screenshot.</returns>
+        public Bitmap GetSreenshot()
+        {
+            Graphics g = Graphics.FromImage(bm);
+            g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+
+            return bm;
+        }
+    }
+}
