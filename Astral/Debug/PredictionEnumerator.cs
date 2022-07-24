2c226da17892fc63d2a7133d8c8c02e7ff7c1625
AlizerLaptop <saylent1@gmail.com>
2022-07-24 15:58:27 +0800

    🐛 Added PredictionEnumerator.cs which Rider ignored last push.

---
+++ Astral/Debug/PredictionEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using Astral.Curses;
+using Astral.Detection;
+using Astral.Models;
+using Astral.Models.Configurations;
+using Astral.Utilities;
+using Serilog;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yolov5Net.Scorer;
+
+namespace Astral.Debug
+{
+    public class PredictionEnumerator : IService
+    {
+
+        private readonly ILogger logger;
+
+        const float MinimumConfidence = 0.25f;
+
+        public PredictionEnumerator(IDetectorService model, ILogger logger)
+        {
+            this.logger = logger;
+
+            model.PredictionReceived += PredictionReceived;
+        }
+
+        private void PredictionReceived(object? sender, IEnumerable<PredictionResult> e)
+        {
+            var highConfidenceObjects = e.Where(x => x.Score > MinimumConfidence);
+
+            if (!highConfidenceObjects.Any()) return;
+
+            logger.Debug($"{highConfidenceObjects.Count()} " +
+                $"Objects: {string.Join(", ", highConfidenceObjects.Select(x => x.Label).Distinct())}");
+        }
+    }
+}
